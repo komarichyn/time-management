4f64d8527343f3163d5b47ccbc20a1d40ac5fe32
FloziK <nazaro4ekk@gmail.com>
2021-06-02 14:39:49 +0300

    CommentDaoImpl.java fixed values CommentSubMenu.java added logs

--- src/main/java/com/jc/tm/ui/subMenu/CommentSubMenu.java
+++ src/main/java/com/jc/tm/ui/subMenu/CommentSubMenu.java
@@ -3,9 +3,11 @@ package com.jc.tm.ui.subMenu;
 import com.jc.tm.database.entity.Comment;
 import com.jc.tm.service.ITaskService;
 import com.jc.tm.ui.console.MyDevice;
+import lombok.extern.slf4j.Slf4j;
 
 import java.sql.SQLException;
 
+@Slf4j
 public class CommentSubMenu {
     private MyDevice console;
     private ITaskService service;
@@ -16,6 +18,7 @@ public class CommentSubMenu {
     }
 
     public void createComment() {
+        log.debug("createComment: in CommentSubMenu");
         var comment = new Comment();
         console.printf("set task id%n");
         Long taskId = Long.parseLong(console.readLine());
@@ -24,13 +27,16 @@ public class CommentSubMenu {
         comment.setTaskId(taskId);
         comment.setText(text);
         try {
+            log.debug("createComment: comment from user {}", comment);
             service.addComment(taskId, comment);
         } catch (SQLException e) {
-            console.printf("wrong input ");
+            log.error("createComment: problem with {}", comment);
+            e.printStackTrace();
         }
     }
 
     public void updateComment() {
+        log.debug("updateComment: in CommentSubMenu");
         var comment = new Comment();
         console.printf("id of the comment we want to update%n");
         Long id = Long.parseLong(console.readLine());
@@ -39,18 +45,23 @@ public class CommentSubMenu {
         comment.setId(id);
         comment.setText(newText);
         try {
+            log.debug("updateComment: comment from user {}", comment);
             service.updateComment(comment);
         } catch (SQLException e) {
-            console.printf("wrong input ");
+            log.error("updateComment: problem with {}" , comment);
+            e.printStackTrace();
         }
     }
 
     public void removeComment() {
+        log.debug("removeComment: in CommentSubMenu");
         console.printf("Enter comment id to delete: ");
         Long id = Long.parseLong(console.readLine());
         try {
+            log.debug("removeComment: id from user {}", id);
             service.removeComment(id);
         } catch (SQLException e) {
+            log.error("removeComment: problem with {}", id);
             e.printStackTrace();
         }
     }
