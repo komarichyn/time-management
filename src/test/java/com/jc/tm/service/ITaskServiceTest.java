1586a607540264cb0013687387ef445813241b66
mescyura <mescyura@gmail.com>
2021-07-07 22:11:13 +0300

    Added H2 for testing the first test is implemented (I don't know if I'm moving in the right direction with the tests)

---
+++ src/test/java/com/jc/tm/service/ITaskServiceTest.java
@@ -0,0 +1,142 @@
+package com.jc.tm.service;
+
+import com.jc.tm.db.Status;
+import com.jc.tm.db.dao.jpa.CommentDao;
+import com.jc.tm.db.dao.jpa.TaskDao;
+import com.jc.tm.db.entity.Task;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+
+@ExtendWith(SpringExtension.class)
+@DataJpaTest
+@PropertySource("classpath:application.properties")
+//@ContextConfiguration(classes = {DbConfig.class})
+public class ITaskServiceTest {
+
+    @Autowired
+    private TaskDao taskDao;
+    @Autowired
+    private CommentDao commentDao;
+
+    private TaskServiceImpl taskService;
+
+    @BeforeEach
+    void setUp() {
+        taskService = new TaskServiceImpl(taskDao, commentDao);
+
+    }
+
+    private Task generateTask() {
+        Task task = new Task();
+        task.setName("test");
+        task.setDescription("some description");
+        task.setStatus(Status.TODO);
+        task.setPriority(Priority.NORMAL);
+        return task;
+    }
+
+    @Test
+    void saveTask() {
+        Object result = taskService.saveTask(generateTask());
+        assertThat(result).isNotNull();
+    }
+
+    @Test
+    void removeTask() {
+    }
+
+    @Test
+    void removeTask1() {
+    }
+
+    @Test
+    void updateTask() {
+    }
+
+    @Test
+    void getTask() {
+    }
+
+    @Test
+    void getTask1() {
+    }
+
+    @Test
+    void loadTasks() {
+    }
+
+    @Test
+    void loadTasks1() {
+    }
+
+    @Test
+    void loadTasksByDescPriority() {
+    }
+
+    @Test
+    void loadTasksByAskPriority() {
+    }
+
+    @Test
+    void addComment() {
+    }
+
+    @Test
+    void addComment1() {
+    }
+
+    @Test
+    void removeComment() {
+    }
+
+    @Test
+    void removeComment1() {
+    }
+
+    @Test
+    void updateComment() {
+    }
+
+    @Test
+    void setDueDate() {
+    }
+
+    @Test
+    void setDueDate1() {
+    }
+
+    @Test
+    void updateDueDate() {
+    }
+
+    @Test
+    void updateDueDate1() {
+    }
+
+    @Test
+    void setPriority() {
+    }
+
+    @Test
+    void setParentTask() {
+    }
+
+    @Test
+    void moveTaskToRoot() {
+    }
+
+    @Test
+    void toPauseState() {
+    }
+
+    @Test
+    void setState() {
+    }
+}
