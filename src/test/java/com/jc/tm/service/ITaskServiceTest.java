9a74324ebf78b5a8ce807bd58a2e71d740618b10
mescyura <mescyura@gmail.com>
2021-07-11 19:56:38 +0300

    refactor tests not done yet

--- src/test/java/com/jc/tm/service/ITaskServiceTest.java
+++ src/test/java/com/jc/tm/service/ITaskServiceTest.java
@@ -2,133 +2,83 @@ package com.jc.tm.service;
 
 import com.jc.tm.db.H2Config;
 import com.jc.tm.db.Status;
+import com.jc.tm.db.dao.jpa.CommentDao;
+import com.jc.tm.db.dao.jpa.TaskDao;
 import com.jc.tm.db.entity.Task;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.verify;
 
 
-@ExtendWith(SpringExtension.class)
-@ContextConfiguration(classes = {H2Config.class})
+@ExtendWith(MockitoExtension.class)
+//@ContextConfiguration(classes = {H2Config.class})
+/*
+I commented this annotation because :
+    If I understand right how mocks work
+    than we don't need connection to db because we test service layer and we mocked the dao layer
+    we will use H2Config when we test our oun custom methods in dao
+    is that right?
+ */
 public class ITaskServiceTest {
 
-    @Autowired
-    private ITaskService taskService;
+    @Mock
+    private TaskDao taskDao;
+    @Mock
+    private CommentDao commentDao;
+
+    @InjectMocks
+    private TaskServiceImpl taskService;
 
     @BeforeEach
     void setUp() {
-
-    }
-
-    private Task generateTask() {
-        Task task = new Task();
-        task.setName("test");
-        task.setDescription("some description");
-        task.setStatus(Status.TODO);
-        task.setPriority(Priority.NORMAL);
-        return task;
+        taskService = new TaskServiceImpl(taskDao, commentDao);
     }
 
     @Test
     void saveTask() {
-        Object result = taskService.saveTask(generateTask());
-        assertThat(result).isNotNull();
+        //given
+        Task task = new Task();
+        task.setId(1L);
+        task.setName("test");
+        task.setDescription("some description");
+        task.setStatus(Status.TODO);
+        task.setPriority(Priority.NORMAL);
+        //when
+        Mockito.when(taskDao.save(Mockito.any(Task.class))).thenReturn(task);
+        //then
+        task = taskService.saveTask(task);
+        assertThat(task).isNotNull();
+        assertNotNull(task.getId());
     }
 
     @Test
-    void removeTask() {
+    @Disabled
+    //not working yet
+    void removeTask(){
+        Task task = new Task();
+        task.setId(1L);
+        taskService.removeTask(task);
+        verify(taskService.removeTask(taskDao.getById(task.getId())));
     }
 
     @Test
-    void removeTask1() {
+    void loadAllTasks() {
+        //when
+        taskService.loadTasks();
+        //then
+        verify(taskDao).findAll();
     }
 
-    @Test
-    void updateTask() {
-    }
 
-    @Test
-    void getTask() {
-    }
 
-    @Test
-    void getTask1() {
-    }
-
-    @Test
-    void loadTasks() {
-    }
-
-    @Test
-    void loadTasks1() {
-    }
-
-    @Test
-    void loadTasksByDescPriority() {
-    }
-
-    @Test
-    void loadTasksByAskPriority() {
-    }
-
-    @Test
-    void addComment() {
-    }
-
-    @Test
-    void addComment1() {
-    }
-
-    @Test
-    void removeComment() {
-    }
-
-    @Test
-    void removeComment1() {
-    }
-
-    @Test
-    void updateComment() {
-    }
-
-    @Test
-    void setDueDate() {
-    }
-
-    @Test
-    void setDueDate1() {
-    }
-
-    @Test
-    void updateDueDate() {
-    }
-
-    @Test
-    void updateDueDate1() {
-    }
-
-    @Test
-    void setPriority() {
-    }
-
-    @Test
-    void setParentTask() {
-    }
-
-    @Test
-    void moveTaskToRoot() {
-    }
-
-    @Test
-    void toPauseState() {
-    }
-
-    @Test
-    void setState() {
-    }
 }
