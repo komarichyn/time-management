cd6b987b792da724df97205ee31d99c886c90aa2
Mykhaylo Komarichyn <mykhaylo.komarichyn@unicorn.com>
2021-07-12 12:06:06 +0300

    how to use mocks

--- src/test/java/com/jc/tm/service/TaskServiceMockTest.java
+++ src/test/java/com/jc/tm/service/TaskServiceMockTest.java
@@ -11,9 +11,17 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.context.config.ConfigData;
+
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class TaskServiceMockTest {
@@ -43,10 +51,31 @@ public class TaskServiceMockTest {
     public void saveTaskTest(){
         Task task = new Task();
         task.setId(1l);
-        Mockito.when(taskDao.save(Mockito.any(Task.class))).thenReturn(task);
+        when(taskDao.save(any(Task.class))).thenReturn(task);
 
         task = taskService.saveTask(task);
         assertThat(task).isNotNull();
         assertNotNull(task.getId());
+    }
+
+    @Test
+    @DisplayName("Negative test for remove task")
+    public void removeTaskNegativeTest(){
+        Task task = new Task();
+        task.setId(1L);
+        when(taskDao.findById(anyLong())).thenReturn(Optional.empty());
+        task = taskService.removeTask(2L);
+        assertNull(task);
+    }
+
+    @Test
+    @DisplayName("Positive test for remove task")
+    public void removeTaskPositiveTest(){
+        Task task = new Task();
+        Long id = 1L;
+        task.setId(id);
+        when(taskDao.findById(id)).thenReturn(Optional.of(task));
+        task = taskService.removeTask(id);
+        assertNotNull(task);
     }
 }
