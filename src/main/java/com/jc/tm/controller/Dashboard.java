f60eebb7c9364849de57210d245e9686e27b8672
mk <mkomarichin@introproventures.com>
2021-07-14 12:23:57 +0300

    Merge pull request #61
    * migrate to mvc

--- src/main/java/com/jc/tm/controller/Dashboard.java
+++ src/main/java/com/jc/tm/controller/Dashboard.java
@@ -11,4 +11,9 @@ public class Dashboard {
     public String greeting(Model model) {
         return "welcome";
     }
+
+    @GetMapping("/my")
+    public String heelo(Model model) {
+        return "my";
+    }
 }
