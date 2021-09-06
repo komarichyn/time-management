1044bfe6d9b89a180ced4619f10434b491095c87
Flozik <49100972+Flozik@users.noreply.github.com>
2021-09-06 13:35:45 +0300

    Code cleaning (#66)
    Merge pull request #66
    * create-task.html some changes with tags
    * Back changes to previous
    * Task.java AUTO vs IDENTITY. Win IDENTITY. With IDENTITY program work …

--- src/main/java/com/jc/tm/controller/Dashboard.java
+++ src/main/java/com/jc/tm/controller/Dashboard.java
@@ -1,11 +1,20 @@
 package com.jc.tm.controller;
 
+import com.jc.tm.db.entity.Task;
 import com.jc.tm.service.PaginationDto;
 import com.jc.tm.service.TaskServiceImpl;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
+/**
+ * this class is controller and it merge database with UI
+ */
+
+@Slf4j
 @Controller
 public class Dashboard {
     private final TaskServiceImpl service;
@@ -16,38 +25,39 @@ public class Dashboard {
 
     @GetMapping("/show-tasks")
     public String show(Model model) {
+        log.debug("show tasks page");
         PaginationDto paginationDto = new PaginationDto();
-        service.loadTasks(paginationDto);
-        model.addAttribute("service", service.loadTasks());
+        model.addAttribute("service", service.loadTasks(paginationDto));
         return "show-tasks";
-//        PaginationDto paginationDto = new PaginationDto();
-        /*return new ResponseEntity<>(service.sortedByNameDESCTasks(paginationDto).stream().
-                map(x->x.toString()).collect(Collectors.joining(", ")), HttpStatus.OK);*/
     }
 
     @GetMapping("/create-task")
     public String create(Model model) {
-        model.addAttribute("myAtribute", "Petja");
-        PaginationDto paginationDto = new PaginationDto();
-        paginationDto.setSize(10);
-        paginationDto.setSortByName("my Field");
-        paginationDto.setPage(1);
-        model.addAttribute("myModel", paginationDto);
+        log.debug("create task page");
         return "create-task";
+    }
+
+    @PostMapping("/add-task")
+    public String createTask(@ModelAttribute Task task) {
+        service.saveTask(task); //TODO fix bug with date
+        return "redirect:/create-task";
     }
 
     @GetMapping("/update-task")
     public String update(Model model) {
+        log.debug("update task page");
         return "update-task";
     }
 
     @GetMapping("/find-task")
     public String find(Model model) {
+        log.debug("find task page");
         return "find-task";
     }
 
     @GetMapping("/delete-task")
     public String delete(Model model) {
+        log.debug("delete task page");
         return "delete-task";
     }
 }
--- src/main/java/com/jc/tm/db/entity/Task.java
+++ src/main/java/com/jc/tm/db/entity/Task.java
@@ -14,7 +14,7 @@ import java.util.List;
 @Table(name = "task")
 public class Task implements Serializable {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String description;
