ed2fb545d6f76c288b0dde405b15cd8ebd3a65db
Mykhaylo Komarichyn <mykhaylo.komarichyn@unicorn.com>
2021-09-06 14:06:24 +0200

    filter example

--- src/main/java/com/jc/tm/filter/ConvertDateFilter.java
+++ src/main/java/com/jc/tm/filter/ConvertDateFilter.java
@@ -1,27 +1,34 @@
 package com.jc.tm.filter;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.util.StringUtils;
+
 import javax.servlet.*;
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
+@Slf4j
+@Configuration
+@Order(Ordered.HIGHEST_PRECEDENCE)
 public class ConvertDateFilter implements Filter {
-    @Override
-    public void init(FilterConfig filterConfig) throws ServletException {
-        Filter.super.init(filterConfig);
-        System.out.println("inter filter");
-    }
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-        System.out.println(servletRequest.getAttribute("name"));
+        log.debug("{}",servletRequest.getAttribute("name"));
         String data = servletRequest.getParameter("name");
-        System.out.println(data);
-        filterChain.doFilter(servletRequest, servletResponse);
-    }
 
-    @Override
-    public void destroy() {
-        Filter.super.destroy();
-        System.out.println("destroy filter");
+
+        String sDueDate = servletRequest.getParameter("dueDate");
+        if(StringUtils.hasText(sDueDate)) {
+            LocalDateTime dueDate = LocalDateTime.parse(sDueDate, DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm"));
+            log.info("due date is: {}", dueDate);
+        }
+
+        log.debug(data);
+        filterChain.doFilter(servletRequest, servletResponse);
     }
 }
