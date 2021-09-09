767dfc9511f435db2f2ada1ef3007046702d78a9
Flozik <49100972+Flozik@users.noreply.github.com>
2021-09-09 10:42:02 +0300

    Code/cleaning (#67)
    * create-task.html some changes with tags
    TaskServiceImpl.java added logs
    PaginationDto.java deleted commented lines
    Dashboard.java added PostMapping for add task and some changes in methods
    
    * Back changes to previous
    
    * Task.java AUTO vs IDENTITY. Win IDENTITY. With IDENTITY program work correctly
    create-task.html name must be with "_" it work with it
    
    * Filter
    index.html don't work table
    
    * filter example
    
    * index.html blank page
    show-tasks.html a few modifications
    index.html new file in templates
    TaskServiceImpl.java LocalDateTime added to curent time
    new file TaskDto.java
    Filter for web ConvertDateFilter.java
    Dashboard.java convert LocalDateTime to String
    
    Co-authored-by: Mykhaylo Komarichyn <mykhaylo.komarichyn@unicorn.com>
    Co-authored-by: Yurii Holyda <41732864+mescyura@users.noreply.github.com>

--- src/main/java/com/jc/tm/controller/Dashboard.java
+++ src/main/java/com/jc/tm/controller/Dashboard.java
@@ -2,6 +2,7 @@ package com.jc.tm.controller;
 
 import com.jc.tm.db.entity.Task;
 import com.jc.tm.service.PaginationDto;
+import com.jc.tm.service.TaskDto;
 import com.jc.tm.service.TaskServiceImpl;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
@@ -9,6 +10,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Collection;
 
 /**
  * this class is controller and it merge database with UI
@@ -23,11 +28,35 @@ public class Dashboard {
         this.service = service;
     }
 
+    @GetMapping("/index")
+    public String mainPage(Model model) {
+        log.debug("show last five tasks");
+        PaginationDto paginationDto = new PaginationDto();
+        paginationDto.setPage(1);
+        paginationDto.setSize(5);
+        model.addAttribute("lastFive", service.loadTasks(paginationDto));
+        return "index";
+    }
+
     @GetMapping("/show-tasks")
     public String show(Model model) {
         log.debug("show tasks page");
         PaginationDto paginationDto = new PaginationDto();
-        model.addAttribute("service", service.loadTasks(paginationDto));
+        paginationDto.setPage(1);
+        paginationDto.setSize(5);
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm");
+        Collection<Task> taskList = service.loadTasks(paginationDto);
+        Collection<TaskDto> result = new ArrayList<>();
+        for(Task task:taskList) {
+            TaskDto taskDto = new TaskDto();
+            LocalDateTime date = task.getDueDate();
+            String formatedDate = date.format(dateTimeFormatter);
+            taskDto.setDueDate(formatedDate);
+            taskDto.setName(task.getName());
+            taskDto.setPriority(task.getPriority());
+            result.add(taskDto);
+        }
+        model.addAttribute("service", result);
         return "show-tasks";
     }
 
---
+++ src/main/java/com/jc/tm/filter/ConvertDateFilter.java
@@ -0,0 +1,38 @@
+package com.jc.tm.filter;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.util.StringUtils;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@Slf4j
+@Configuration
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class ConvertDateFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        log.debug("{}",servletRequest.getAttribute("name"));
+        String data = servletRequest.getParameter("name");
+
+
+        String sDueDate = servletRequest.getParameter("dueDate");
+        if(StringUtils.hasText(sDueDate)) {
+            LocalDateTime dueDate = LocalDateTime.parse(sDueDate, DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm"));
+            log.info("due date is: {}", dueDate);
+        }
+        log.info(((HttpServletRequest)servletRequest).getServletPath());
+//        if(servletResponse.set)
+
+        log.debug(data);
+        filterChain.doFilter(servletRequest, servletResponse);
+        log.info("en");
+    }
+}
