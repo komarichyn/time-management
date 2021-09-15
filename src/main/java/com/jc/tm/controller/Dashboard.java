2b009cd2c9761f95a0b813cbe1ebb2005475e4f5
Yurii <mescyura@gmail.com>
2021-09-15 17:54:20 +0300

    redone the date converter method some small refactor

--- src/main/java/com/jc/tm/controller/Dashboard.java
+++ src/main/java/com/jc/tm/controller/Dashboard.java
@@ -1,6 +1,5 @@
 package com.jc.tm.controller;
 
-import com.jc.tm.db.dao.jpa.TaskDao;
 import com.jc.tm.db.entity.Task;
 import com.jc.tm.service.PaginationDto;
 import com.jc.tm.service.TaskDto;
@@ -10,13 +9,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import java.text.CollationElementIterator;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.List;
-import java.util.Map;
 
 /**
  * this class is controller and it merge database with UI
@@ -26,32 +22,37 @@ import java.util.Map;
 @Controller
 public class Dashboard {
     private final TaskServiceImpl service;
+    private DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm");
 
     public Dashboard(TaskServiceImpl service) {
         this.service = service;
     }
 
-    private Collection<TaskDto> dateConvert(Collection<Task> tasks) {
-        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm");
+    private Collection<TaskDto> parsingTaskDataToTaskDTO(Collection<Task> tasks) {
         Collection<TaskDto> result = new ArrayList<>();
         for (Task task : tasks) {
-            TaskDto taskDto = new TaskDto();
-            LocalDateTime dateCreated = task.getCreated();
-            LocalDateTime dateDueDate = task.getDueDate();
-            if (dateCreated != null) {
-                String formattedDateCreated = dateCreated.format(dateTimeFormatter);
-                taskDto.setCreated(formattedDateCreated);
-            }
-            String formattedDateDueDate = dateDueDate.format(dateTimeFormatter);
-            taskDto.setDueDate(formattedDateDueDate);
-            taskDto.setId(task.getId());
-            taskDto.setName(task.getName());
-            taskDto.setDescription(task.getDescription());
-            taskDto.setStatus(task.getStatus());
-            taskDto.setPriority(task.getPriority());
+            TaskDto taskDto = dateConvert(task);
             result.add(taskDto);
         }
         return result;
+    }
+
+    private TaskDto dateConvert(Task task) {
+        TaskDto taskDto = new TaskDto();
+        LocalDateTime dateCreated = task.getCreated();
+        LocalDateTime dateDueDate = task.getDueDate();
+        if (dateCreated != null) {
+            String formattedDateCreated = dateCreated.format(dateTimeFormatter);
+            taskDto.setCreated(formattedDateCreated);
+        }
+        String formattedDateDueDate = dateDueDate.format(dateTimeFormatter);
+        taskDto.setDueDate(formattedDateDueDate);
+        taskDto.setId(task.getId());
+        taskDto.setName(task.getName());
+        taskDto.setDescription(task.getDescription());
+        taskDto.setStatus(task.getStatus());
+        taskDto.setPriority(task.getPriority());
+        return taskDto;
     }
 
     @GetMapping("/index")
@@ -61,7 +62,7 @@ public class Dashboard {
         paginationDto.setPage(0);
         paginationDto.setSize(5);
         Collection<Task> taskList = service.sortedByDueDateDESCTasks(paginationDto);
-        Collection<TaskDto> result = dateConvert(taskList);
+        Collection<TaskDto> result = parsingTaskDataToTaskDTO(taskList);
         model.addAttribute("lastFive", result);
         return "index";
     }
@@ -73,7 +74,7 @@ public class Dashboard {
         paginationDto.setPage(0);
         paginationDto.setSize(20);
         Collection<Task> taskList = service.loadTasks(paginationDto);
-        Collection<TaskDto> result = dateConvert(taskList);
+        Collection<TaskDto> result = parsingTaskDataToTaskDTO(taskList);
         model.addAttribute("service", result);
         return "show-tasks";
     }
@@ -92,22 +93,16 @@ public class Dashboard {
 
     @GetMapping("/task/{taskId}")
     public String getTaskById(Model model, @PathVariable long taskId) {
-        Task task = null;
-        task = service.getTask(taskId);
-        model.addAttribute("task", task);
+        Task task = service.getTask(taskId);
+        TaskDto taskDto = dateConvert(task);
+        model.addAttribute("task", taskDto);
         return "task";
     }
 
-    @GetMapping("/update-task")
+    @GetMapping("/update-task") // TODO update method
     public String update(Model model) {
         log.debug("update task page");
         return "update-task";
-    }
-
-    @GetMapping("/find-task")
-    public String find(Model model) {
-        log.debug("find task page");
-        return "find-task";
     }
 
     @GetMapping("/delete-task/{taskId}")
