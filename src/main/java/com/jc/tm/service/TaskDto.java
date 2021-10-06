5901b1e3c970ef899cf40bfd875d976b90d9f86f
Yurii <mescyura@gmail.com>
2021-10-06 19:45:42 +0300

    updated task view added Bootstrap transferred Converter to an converter package

--- src/main/java/com/jc/tm/service/TaskDto.java
+++ src/main/java/com/jc/tm/service/TaskDto.java
@@ -1,7 +1,11 @@
 package com.jc.tm.service;
 
 import com.jc.tm.db.Status;
+import com.jc.tm.db.entity.Comment;
 import lombok.Data;
+
+import java.util.Collection;
+import java.util.List;
 
 @Data
 public class TaskDto {
@@ -9,6 +13,7 @@ public class TaskDto {
     private String name;
     private String description;
     private String created;
+    private Collection<CommentDto> comments;
     private Status status;
     private String dueDate;
     private Priority priority = Priority.NORMAL;
