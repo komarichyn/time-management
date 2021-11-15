151a76bde016d2a27cfe3b67b42cb3aa657f41c7
Yurii Holyda <41732864+mescyura@users.noreply.github.com>
2021-11-15 22:41:05 +0200

    replaced lombok builder with own static builder (#86)

--- src/main/java/com/jc/tm/db/entity/Comment.java
+++ src/main/java/com/jc/tm/db/entity/Comment.java
@@ -1,7 +1,6 @@
 package com.jc.tm.db.entity;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -11,20 +10,54 @@ import java.time.LocalDateTime;
 
 @Data
 @Entity
-@Table(name =  "comment")
-@Builder
+@Table(name = "comment")
 @NoArgsConstructor
 @AllArgsConstructor
 public class Comment implements Serializable {
     @Id
-    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name="task_id", nullable=false)
+    @JoinColumn(name = "task_id", nullable = false)
     private Task task;
     @Column(columnDefinition = "TEXT")
     private String text;
     @Column(columnDefinition = "TIMESTAMP")
     private LocalDateTime created;
 
+    public static CommentBuilder builder() {
+        return new CommentBuilder();
+    }
+
+    public static class CommentBuilder {
+        private Comment comment;
+
+        public CommentBuilder() {
+            comment = new Comment();
+        }
+
+        public CommentBuilder setId(Long id) {
+            comment.id = id;
+            return this;
+        }
+
+        public CommentBuilder setTask(Task task) {
+            comment.task = task;
+            return this;
+        }
+
+        public CommentBuilder setText(String text) {
+            comment.text = text;
+            return this;
+        }
+
+        public CommentBuilder setCreated(LocalDateTime created) {
+            comment.created = created;
+            return this;
+        }
+
+        public Comment build() {
+            return comment;
+        }
+    }
 }
--- src/main/java/com/jc/tm/db/entity/Task.java
+++ src/main/java/com/jc/tm/db/entity/Task.java
@@ -3,7 +3,6 @@ package com.jc.tm.db.entity;
 import com.jc.tm.db.Status;
 import com.jc.tm.service.Priority;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -15,7 +14,6 @@ import java.util.List;
 @Data
 @Entity
 @Table(name = "task")
-@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class Task implements Serializable {
@@ -29,14 +27,67 @@ public class Task implements Serializable {
     @OneToMany(mappedBy = "task", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Comment> comments;
     @Enumerated(EnumType.STRING)
-    @Builder.Default
     //default status for any task is to do
     private Status status = Status.TODO;
     @Column(columnDefinition = "TIMESTAMP", name = "due_date")
     private LocalDateTime dueDate;
     @Enumerated(EnumType.STRING)
-    @Builder.Default
     //default priority for any task is normal
     private Priority priority = Priority.NORMAL;
 
+    public static TaskBuilder builder(){
+        return new TaskBuilder();
+    }
+
+    public static class TaskBuilder {
+        private Task task;
+
+        public TaskBuilder() {
+            task = new Task();
+        }
+
+        public TaskBuilder setId(long id) {
+            task.id = id;
+            return this;
+        }
+
+        public TaskBuilder setName(String name) {
+            task.name = name;
+            return this;
+        }
+
+        public TaskBuilder setDescription(String description) {
+            task.description = description;
+            return this;
+        }
+
+        public TaskBuilder setCreated(LocalDateTime created) {
+            task.created = created;
+            return this;
+        }
+
+        public TaskBuilder setComments(List<Comment> comments) {
+            task.comments = comments;
+            return this;
+        }
+
+        public TaskBuilder setStatus(Status status) {
+            task.status = status;
+            return this;
+        }
+
+        public TaskBuilder setDueDate(LocalDateTime dueDate) {
+            task.dueDate = dueDate;
+            return this;
+        }
+
+        public TaskBuilder setPriority(Priority priority) {
+            task.priority = priority;
+            return this;
+        }
+
+        public Task build() {
+            return task;
+        }
+    }
 }
